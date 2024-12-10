fe04b1437d8ad1cd08a5e19d18ef6e27409dfe86
shivanagendrak <shivaaman751@gmail.com>
2024-12-10 12:04:59 -0600

    Views.py updated

--- Breezyapp/views.py
+++ Breezyapp/views.py
@@ -1,62 +1,7 @@
-# import requests
-# from django.shortcuts import render 
-# from django.conf import settings
-
-# def current_weather(request): #request: Represents the HTTP request made to this view.
-#     city=request.GET.get("city","New York")
-#     unit=request.GET.get("unit","imperial")
-#     unit_symbol="°F'" if unit== "imperial" else "°C"
-#     api_key=settings.OPENWEATHER_API_KEY
-#     current_weather_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units={unit}'
-#     forecast_url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units={unit}'
-#     current_weather_response=requests.get(current_weather_url)
-#     current_weather_data=current_weather_response.json()
-#     forecast_response=requests.get(forecast_url)
-#     forecast_data=forecast_response.json()
-    
-#     temperature=current_weather_data.get("main",{}).get("temp","N/A")
-#     description = current_weather_data.get('weather', [{}])[0].get('description', 'N/A').capitalize()
-#     icon=current_weather_data.get("weather",[{}])[0].get("icon","N/A")
-
-#     context={
-#         "city":city,
-#         "temperature":temperature,
-#         "description":description,
-#         "icon":icon,
-#         "unit_symbol":unit_symbol                
-#     }
-
-#     return render(request,"current_weather.html",context)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import requests
 from django.shortcuts import render
-from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response 
 from django.conf import settings
+from datetime import datetime
 
 def current_weather(request):
     city = request.GET.get('city', 'New York')  # Default city
@@ -74,20 +19,27 @@ def current_weather(request):
     forecast_response = requests.get(forecast_url)
     forecast_data = forecast_response.json()
 
+    # Get today's date to exclude it from the forecast
+    today = datetime.now().date()
+
     # Process forecast data: Group by date and select one forecast (closest to midday)
     daily_forecasts = {}
-    for forecast in forecast_data['list']:
-        date = forecast['dt_txt'].split(' ')[0]  # Extract date (YYYY-MM-DD)
-        time = forecast['dt_txt'].split(' ')[1]  # Extract time (HH:MM:SS)
-        if date not in daily_forecasts or time == '12:00:00':
-            daily_forecasts[date] = {
-                'datetime': forecast['dt_txt'],
-                'temperature': forecast['main']['temp'],
-                'description': forecast['weather'][0]['description'],
-                'icon': forecast['weather'][0]['icon'],
-            }
-        if len(daily_forecasts) == 5:  # Limit to 5 unique days
-            break
+    for forecast in forecast_data.get('list', []):
+        date_str = forecast['dt_txt'].split(' ')[0]  # Extract date (YYYY-MM-DD)
+        time_str = forecast['dt_txt'].split(' ')[1]  # Extract time (HH:MM:SS)
+        forecast_date = datetime.strptime(date_str, '%Y-%m-%d').date()
+
+        if forecast_date > today:  # Exclude today's date
+            if date_str not in daily_forecasts or time_str == '12:00:00':  # Closest to midday
+                daily_forecasts[date_str] = {
+                    'datetime': forecast['dt_txt'],
+                    'date': forecast_date,
+                    'temperature': forecast['main']['temp'],
+                    'description': forecast['weather'][0]['description'].capitalize(),
+                    'icon': forecast['weather'][0]['icon'],
+                }
+            if len(daily_forecasts) == 5:  # Limit to 5 unique days
+                break
 
     forecast_list = list(daily_forecasts.values())
 
